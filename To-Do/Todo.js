503d17b88f2d697cc4f95186c46a252b05b21bb8
Adora Gillespie <adora.mae.jeanne@gmail.com>
2015-10-27 19:56:09 -0500

    To Do List Mini App

---
+++ To-Do/Todo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(document).ready(function() {
+	$(this).on('submit', function(event){
+		event.preventDefault();
+		var item = $('#todo').val();
+		$('#todo-list').append('<li>' + item + '</li>');
+		var item = $('#todo').val('');
+	});
+
+	$(this).on('dblclick', 'li', function(event){
+		event.preventDefault();
+		$(this).remove();
+
+	});
+});
